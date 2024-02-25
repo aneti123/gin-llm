dbb8cefbc46708241ec39a7859a5ccceff6eb723
Anirudh Neti <anirudh.neti@gmail.com>
2024-02-25 11:47:08 +0000

    added gpu measuring

--- src/main/java/gin/test/TestRunListener.java
+++ src/main/java/gin/test/TestRunListener.java
@@ -44,7 +44,7 @@ public class TestRunListener implements Serializable, TestExecutionListener {
             Logger.debug("Test " + testIdentifier.getDisplayName() + " finished.");
             EnergyStats endEnergySample = energyMonitor.getSample();
             EnergyDiff diff = EnergyDiff.between(this.startEnergySample, endEnergySample);
-            double energyDifference = diff.getPackage();
+            double energyDifference = diff.getPackage() + diff.getGpu();
             energyMonitor.deactivate();
             unitTestResult.setEnergyUsage(energyDifference);
 
